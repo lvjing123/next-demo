8ef9fe25f626b464e47b19f37bdf1bf0a4acc7ad
lvjing123 <70423556+lvjing123@users.noreply.github.com>
2024-02-29 17:53:45 +0800

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,13 @@
+// 可以从next 引入一些组件，例如 image link
+import Image from "next/image"
+import Link from 'next/link'
+// 可以从其他地方引入一些样式
+import styles from '@/app/ui/home.module.css'
 
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className={styles.shape}></div>
+    </main>
+  )
+}

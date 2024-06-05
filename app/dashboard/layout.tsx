e071a2f356bee1a9f71f8b913cd2d72659e6dc8c
lvscy <70423556+lvjing123@users.noreply.github.com>
2024-06-05 13:38:42 +0800

    add local pgsql

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,27 @@
 import SideNav from '@/app/ui/dashboard/sidenav'
+import { signOut } from '../../auth'
+import styles from '@/app/ui/css/home.module.css'
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
+    <>
+      <form
+            action={async () => {
+              'use server';
+              await signOut();
+            }}
+          >
+            <button className={`flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 ${styles['header-conatiner']}` }>
+              <div className="hidden md:block">Sign Out</div>
+            </button>
+      </form>
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <SideNav />
+          </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-    </div>
+    </>
+    
   )
 }

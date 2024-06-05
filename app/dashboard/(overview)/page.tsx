f23736c089be75d1b1c558bae772214c5f0cee4c
lvscy <70423556+lvjing123@users.noreply.github.com>
2024-06-05 13:35:06 +0800

    add local pgsql

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-                Dashboard
+                home 首页
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
                 <Suspense fallback={<CardsSkeleton />}>

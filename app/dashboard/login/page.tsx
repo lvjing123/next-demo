1bb0e2f291a4cebe8b88a07d5aa08d8bc23d7737
lvscy <70423556+lvjing123@users.noreply.github.com>
2024-06-05 13:58:21 +0800

    Create login page.tsx

---
+++ app/dashboard/login/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+// login page
+import LoginForm from '@/app/ui/login-form'
+
+export default function LoginPage() {
+    return (
+      <main className="flex items-center justify-center md:h-screen">
+        <LoginForm />
+      </main>
+    )
+}

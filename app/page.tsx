a1916fc6f3e5accaa1c800b88eec7e68a64d12aa
lvscy <70423556+lvjing123@users.noreply.github.com>
2024-06-05 13:46:27 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,16 @@ import Image from "next/image"
 import Link from 'next/link'
 // 可以从其他地方引入一些样式
 import styles from '@/app/ui/home.module.css'
+// 引入 login
+import Login from '@/app/dashboard/login/page'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className={styles.shape}></div>
+      {/* <div className={styles.shape}> */}
+        {/* 引入css 中某个css 样式的例子 */}
+      {/* </div> */}
+      <Login />
     </main>
   )
 }
